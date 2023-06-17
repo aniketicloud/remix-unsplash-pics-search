1066d34061946b5ff705dd6d0f3fcda4c1e551d3
Aniket Pandharabale <aniket.pandhararbale@yash.com>
2023-06-18 00:46:02 +0530

    checking unsplash developer access

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,36 @@
 import type { V2_MetaFunction } from "@remix-run/node";
+import SearchBar from "~/components/SearchBar";
+import ImageList from "~/components/ImageList";
+import axios from "axios";
 
 export const meta: V2_MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "Unsplash Image Search" },
+    {
+      name: "Search images from Unsplash",
+      content: "Search images from Unsplash",
+    },
   ];
+};
+
+const searchImages = async () => {
+  const results = await axios.get("https://api.unsplash.com/search/photos", {
+    headers: {
+      Authorization: "Client-ID JoDf_SrKGzyaMG6ljqTIgubhFppAYzErp-t5pqEZQ6w",
+    },
+    params: {
+      query: "car",
+    },
+  });
+  console.log(results);
 };
 
 export default function Index() {
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <SearchBar />
+      <ImageList />
+      <button onClick={searchImages}>Search</button>
     </div>
   );
 }
