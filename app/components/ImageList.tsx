06df3d38ce186ca0a62057c02066d585d97eefc9
Aniket Pandharabale <aniketicloud@gmail.com>
2023-07-14 00:46:44 +0530

    rendering imagelist with image show

--- app/components/ImageList.tsx
+++ app/components/ImageList.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import type { Image } from "~/types";
+import ImageShow from "./ImageShow";
 
 interface ImageListProps {
   images?: Image[];
 }
 
 const ImageList: React.FC<ImageListProps> = ({ images }) => {
-  return <h1>{images && images.length}</h1>;
+  const renderedImages = images?.map((image) => (
+    <ImageShow image={image} key={image.id} />
+  ));
+  return <>{images && renderedImages}</>;
 };
 
 export default ImageList;
--- app/components/ImageShow.tsx
+++ app/components/ImageShow.tsx
@@ -1,5 +1,12 @@
-function ImageShow() {
-  return <h1>ImageShow</h1>;
+import React from "react";
+import type { Image } from "~/types";
+
+interface ImageShowProps {
+  image: Image;
 }
+
+const ImageShow: React.FC<ImageShowProps> = ({ image }) => {
+  return <p>{image.id}</p>;
+};
 
 export default ImageShow;
